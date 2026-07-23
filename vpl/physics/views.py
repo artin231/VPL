6f49e3a57b2cccda5772e20538425e1df66adf8f
artin231 <leiartrezjah@gmail.com>
2026-07-23 18:18:07 +0330

    adding a url for the main page and a view

--- vpl/physics/views.py
+++ vpl/physics/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.views.generic.base import TemplateView
 # Create your views here.
+
+
+class HomeSimView(TemplateView):
+    template_name = 'physics/index.html'
+    
